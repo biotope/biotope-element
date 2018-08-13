c59b4a430812ca2dc7285e8d38b0e582b08ac941
Jurek Barth <post@jurekbarth.de>
2018-08-13 11:18:59 +0200

    init 🔥

---
+++ README.md
@@ -0,0 +1,1 @@
+# Biotope Element
---
+++ index.ts
@@ -0,0 +1,37 @@
+import HyperHTMLElement from 'hyperhtml-element';
+
+
+export default class BioElement<TProps extends object, TState> extends HyperHTMLElement<TState> {
+
+  private _props: TProps;
+
+  static get observedAttributes(): string[] {
+    return [];
+  };
+
+  attributeChangedCallback() {
+    this.render();
+  }
+
+  get props(): TProps {
+    return { ...(<any>this.defaultProps), ...(<any>this.propsFromAttributes), ...(<any>this._props) };
+  }
+
+  set props(value) {
+    this._props = value;
+    this.onPropsChanged();
+  }
+
+  get propsFromAttributes(): TProps {
+    return null;
+  }
+
+  get defaultProps(): TProps {
+    return null;
+  }
+
+  // overwrite if you eg need to merge into your state
+  onPropsChanged() {
+    this.render();
+  }
+}
