629a92df38f51abff8d07dfb43502e6dddced8c5
Marc Wißler <marc.wissler@virtual-identity.com>
2018-08-24 12:03:28 +0200

    Change dependencies to static property

--- src/index.ts
+++ src/index.ts
@@ -13,14 +13,7 @@ export default abstract class BioElement<TProps extends object, TState> extends 
   private _props: TProps;
 
   // overwrite to set dependencies
-  get dependencies(): any[] {
-    return [];
-  }
-
-  constructor() {
-    super();
-    this.dependencies.forEach(dep => dep.register());
-  }
+  static dependencies: Function[] = [];
 
   static register(): void {
     const dashedName = dasherize(this.name || this.toString().match(/^function\s*([^\s(]+)/)[1]);
@@ -35,6 +28,11 @@ export default abstract class BioElement<TProps extends object, TState> extends 
   static get observedAttributes(): string[] {
     return this.bioAttributes.map(attributeName);
   };
+
+  constructor() {
+    super();
+    (this.constructor as any).dependencies.forEach((dep: any) => dep.register());
+  }
 
   attributeChangedCallback(name: string, _: string, newValue: string): void {
     const attribute = (this.constructor as any).bioAttributes.find((attr: string) => attributeName(attr) === name);
