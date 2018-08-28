4a14bd51367239ee5f1866de803441d8420bf737
Tiago Marques <tiago.marques@virtual-identity.com>
2018-08-28 16:01:31 +0100

    Fixed component name guessing by adding manual name on static attribute

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import HyperHTMLElement from 'hyperhtml-element';
 import HyperHTML from 'hyperhtml';
-import dasherize from 'dasherize';
 
 import { BioAttribute } from './types';
+import { getComponentName } from './get-component-name';
 import { isRegistered } from './is-registered';
 import { attributeName } from './attribute-name';
 
@@ -15,8 +15,11 @@ export default abstract class BioElement<TProps extends object, TState> extends 
   // overwrite to set dependencies
   static dependencies: Function[] = [];
 
+  // overwrite if you have a minifier/uglifier in your project
+  static componentName: string;
+
   static register(): void {
-    const dashedName = dasherize(this.name || this.toString().match(/^function\s*([^\s(]+)/)[1]);
+    const dashedName = getComponentName(this);
     if (!isRegistered(dashedName)) {
       this.define(dashedName);
     }
