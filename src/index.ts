959adb25377a745819252ceeb41481c865ed1530
Tiago Marques <tiagomapmarques@users.noreply.github.com>
2018-10-17 17:24:26 +0100

    Bugfix: Runtime crash (#37)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import HyperHTMLElement from 'hyperhtml-element';
+import HyperHTMLElement from 'hyperhtml-element/cjs';
 import HyperHTML from 'hyperhtml';
 
 import { Attribute } from './types';
-import { getComponentName } from './get-component-name';
-import { isRegistered } from './is-registered';
+import { register } from './register';
 import { attributeNameMapper } from './attribute-name-mapper';
 
 export { Attribute };
@@ -16,12 +15,7 @@ export default abstract class Component<TProps, TState> extends HyperHTMLElement
   public static componentName: string;
 
   public static register(): void {
-    this.dependencies.forEach(dependency => dependency.register());
-
-    const dashedName = getComponentName(this);
-    if (!isRegistered(dashedName)) {
-      this.define(dashedName);
-    }
+    register(this);
   }
 
   public static get observedAttributes(): string[] {
@@ -30,42 +24,6 @@ export default abstract class Component<TProps, TState> extends HyperHTMLElement
 
   // overwrite if some attributes should be auto-merged to your props
   protected static attributes: (string | Attribute)[] = [];
-
-  private currentProps: TProps;
-
-  constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
-    if (
-      !(this as HTMLElement).getAttributeNames().length
-      || !(this.constructor as typeof Component).attributes
-      || !(this.constructor as typeof Component).attributes.length
-    ) {
-      this.render();
-    }
-  }
-
-  public attributeChangedCallback(name: string, oldValue: string, newValue: string): void {
-    const attribute = (this.constructor as any).attributes
-      .find((attr: string) => attributeNameMapper(attr) === name);
-
-    if (attribute) {
-      this.props = {
-        ...(this.props as any),
-        [name]: typeof attribute === 'string' ? newValue : attribute.converter(newValue),
-      };
-    };
-  }
-
-  protected emit<T>(name: string, detail?: T, addPrefix: boolean = true) {
-    return this.dispatchEvent(new CustomEvent(
-      `${addPrefix ? `${(this.constructor as typeof Component).componentName}-` : ''}${name}`,
-      {
-        bubbles: true,
-        ...(detail !== undefined ? { detail } : {}),
-      },
-    ));
-  }
 
   // overwrite if you want default props in your component
   protected get defaultProps(): TProps {
@@ -92,8 +50,42 @@ export default abstract class Component<TProps, TState> extends HyperHTMLElement
     return HyperHTML.hyper;
   }
 
+  private currentProps: TProps;
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  // overwrite if you, for example, need to fetch something after the component is created
+  public created() {
+    this.render();
+  }
+
+  public attributeChangedCallback(name: string, oldValue: string, newValue: string): void {
+    const attribute = (this.constructor as any).attributes
+      .find((attr: string) => attributeNameMapper(attr) === name);
+
+    if (attribute) {
+      this.props = {
+        ...(this.props as any),
+        [name]: typeof attribute === 'string' ? newValue : attribute.converter(newValue),
+      };
+    };
+  }
+
   // overwrite if you, for example, need to merge props into your state
   protected onPropsChanged() {
     this.render();
+  }
+
+  protected emit<T>(name: string, detail?: T, addPrefix: boolean = true) {
+    return this.dispatchEvent(new CustomEvent(
+      `${addPrefix ? `${(this.constructor as typeof Component).componentName}-` : ''}${name}`,
+      {
+        bubbles: true,
+        ...(detail !== undefined ? { detail } : {}),
+      },
+    ));
   }
 }
