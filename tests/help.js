be4f9c6f53de7633a8394450cd9147aa8a48a36e
SheepFromHeaven <marcwissler@gmail.com>
2018-11-02 15:33:25 +0100

    Add rendering tests

--- tests/help.js
+++ tests/help.js
@@ -1,14 +1,8 @@
 const chai = require('chai');
 const sinonChai = require('sinon-chai');
-const { JSDOM } = require('jsdom');
 
 chai.use(sinonChai);
-const { expect } = chai;
 
-const dom = new JSDOM('<!DOCTYPE html><html><head></head><body></body></html>');
-class HTMLUnknownElement { }
-
-global.window = dom.window;
-global.document = dom.window.document;
-global.HTMLElement = dom.window.HTMLElement;
-global.HTMLUnknownElement = HTMLUnknownElement;
+require('basichtml').init({
+    window: global
+});
---
+++ tests/rendering.spec.ts
@@ -0,0 +1,40 @@
+import sinon from 'sinon';
+import { expect } from 'chai';
+import Component from '../src/index';
+
+
+describe('render', () => {
+
+    it('is called on created', () => {
+        const renderSpy = sinon.spy();
+        class HelloWorld extends Component<object, object> {
+            public static componentName = 'x-world';
+
+            public render = renderSpy;
+        }
+
+        HelloWorld.register()
+
+        const instance = new HelloWorld();
+        instance.created();
+
+        expect(renderSpy).to.have.been.calledOnce;
+    });
+
+    it('is called onPropsChanged by default', () => {
+        const renderSpy = sinon.spy();
+        class HelloWorld extends Component<object, object> {
+            public static componentName = 'x-world';
+
+            public render = renderSpy;
+        }
+
+        HelloWorld.register()
+
+        const instance = new HelloWorld();
+        (instance as any).onPropsChanged();
+
+        expect(renderSpy).to.have.been.calledOnce;
+    });
+})
+
