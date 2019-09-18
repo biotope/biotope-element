93f303fdd2bd425b16c08e6e4c18da7fd98b3f8d
Tiago Marques <tiago.marques@virtual-identity.com>
2019-09-18 15:18:32 +0200

    Add types folder to linting

--- types/basichtml/index.d.ts
+++ types/basichtml/index.d.ts
@@ -1,22 +1,27 @@
+
 declare module 'basichtml' {
-    interface Options {
-        window?: Object,
-        customElements?: typeof customElements,
-        selector?: {
-           name?: string;
-           module?: () => any;
-           $?: (module: any, element: HTMLElement, css: string) => any;
-       }
-    }
-
-    class CustomElementRegistry {
-        define: (name: string, constructor: Function, options: Options) => void;
-    }
-
-    const basicHTML: {
-        init: (options: Options) => typeof Window,
-        CustomElementRegistry: any;
+  interface Options {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    window?: Record<string, any>;
+    customElements?: typeof customElements;
+    selector?: {
+      name?: string;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      module?: () => any;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      $?: (module: any, element: HTMLElement, css: string) => any;
     };
-    export default basicHTML;
+  }
+
+  class CustomElementRegistry {
+    public define: (name: string, constructor: Function, options: Options) => void;
+  }
+
+  const basicHTML: {
+    init: (options: Options) => typeof Window;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    CustomElementRegistry: any;
+  };
+  // eslint-disable-next-line import/no-default-export
+  export default basicHTML;
 }
-    
