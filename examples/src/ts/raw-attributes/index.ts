547c25c5262993b0a64b4f17fb2c3067e148a7e0
Tiago Marques <tiago.marques@virtual-identity.com>
2019-10-30 19:43:10 +0000

    Fix examples

--- examples/src/ts/raw-attributes/index.ts
+++ examples/src/ts/raw-attributes/index.ts
@@ -1,6 +1,11 @@
 import Component from '@biotope/element';
 import { ExampleChild } from './child';
 
+const childCallback = (number?: number): string => {
+  const stringified = number >= 0 ? 'positive' : 'negative';
+  return `Parent says that "${number}" is ${number !== 0 ? stringified : 'zero'}`;
+};
+
 export class ExampleText extends Component {
   public static componentName = 'example-text';
 
@@ -19,18 +24,12 @@ export class ExampleText extends Component {
     return this.html`
       <h1>H1 Headline</h1>
       <example-child
-        my-callback=${this.childCallback}
+        my-callback=${childCallback}
         my-array=${this.myArray}
         my-second-array=${JSON.stringify(this.myArray)}
         my-interpolated="I like the number ${this.randomNumber} very much!"
       ></example-child>
     `;
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  private childCallback(number?: number): string {
-    const stringified = number >= 0 ? 'positive' : 'negative';
-    return `Parent says that "${number}" is ${number !== 0 ? stringified : 'zero'}`;
   }
 }
 
