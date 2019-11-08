79021a47db85f8685ea250abdb75f56f73815b95
Tiago Marques <tiago.marques@virtual-identity.com>
2019-11-08 20:09:10 +0000

    Add feature create ref callback

--- tests/refs.spec.ts
+++ tests/refs.spec.ts
@@ -1,6 +1,6 @@
-import { createRef } from '../src';
+import { createRef, createRefCallback } from '../src';
 
-describe('#refs', () => {
+describe('#createRef', () => {
   it('creates an object', () => {
     const result = createRef<string>();
     expect(result).toBeTruthy();
@@ -15,5 +15,35 @@ describe('#refs', () => {
     const result = createRef<string>();
     result.current = 'mock-string';
     expect(result.current).toBe('mock-string');
+  });
+});
+
+describe('#createRefCallback', () => {
+  let mockGetter: () => string;
+
+  beforeEach(() => {
+    mockGetter = jest.fn(() => 'mock-string');
+  });
+
+  it('creates an object', () => {
+    const result = createRefCallback<string>(mockGetter);
+    expect(result).toBeTruthy();
+  });
+
+  it('creates a reference object already filled', () => {
+    const { current } = createRefCallback<string>(mockGetter);
+    expect(current).toBe('mock-string');
+  });
+
+  it('the object is not assignable', () => {
+    const result = createRefCallback<string>(mockGetter);
+    let caughtError: Error;
+
+    try {
+      result.current = 'mock-string-2';
+    } catch (error) {
+      caughtError = error;
+    }
+    expect(caughtError).toBeTruthy();
   });
 });
