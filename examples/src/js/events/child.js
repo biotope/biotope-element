c3c73a20ba9e3427d4d617b8efd93a57f3e4680e
Tiago Marques <tiago.marques@virtual-identity.com>
2019-11-08 20:39:28 +0000

    Update examples

--- examples/src/js/events/child.js
+++ examples/src/js/events/child.js
@@ -7,21 +7,23 @@ export const ExampleSenderEvents = {
 export class ExampleSender extends Component {
   constructor() {
     super();
-    this.input = createRef();
     this.handleChange = this.handleChange.bind(this);
+    this.refs = {
+      input: createRef(),
+    };
   }
 
   render() {
     return this.html`
       <label>Label</label>
-      <input ref=${this.input} onkeyup=${this.handleChange} type="text" />
+      <input ref=${this.refs.input} onkeyup=${this.handleChange} type="text" />
     `;
   }
 
   handleChange() {
     // eslint-disable-next-line no-console
     console.log('Emitting change on the input…');
-    this.emit(ExampleSenderEvents.change, this.input.current.value);
+    this.emit(ExampleSenderEvents.change, this.refs.input.current.value);
   }
 }
 
--- examples/src/ts/events/child.ts
+++ examples/src/ts/events/child.ts
@@ -1,4 +1,4 @@
-import Component, { createRef, RefObject, HTMLFragment } from '@biotope/element';
+import Component, { createRef, HTMLFragment } from '@biotope/element';
 
 export const ExampleSenderEvents = {
   change: 'examplesender.change',
@@ -9,24 +9,25 @@ export type ExampleSenderChangeType = CustomEvent<string>;
 export class ExampleSender extends Component {
   public static componentName = 'example-sender';
 
-  private input: RefObject<HTMLInputElement>;
+  private refs = {
+    input: createRef<HTMLInputElement>(),
+  };
 
   public constructor() {
     super();
-    this.input = createRef();
     this.handleChange = this.handleChange.bind(this);
   }
 
   public render(): HTMLFragment {
     return this.html`
       <label>Label</label>
-      <input ref=${this.input} onkeyup=${this.handleChange} type="text" />
+      <input ref=${this.refs.input} onkeyup=${this.handleChange} type="text" />
     `;
   }
 
   private handleChange(): void {
     // eslint-disable-next-line no-console
     console.log('Emitting change on the input…');
-    this.emit(ExampleSenderEvents.change, this.input.current.value);
+    this.emit(ExampleSenderEvents.change, this.refs.input.current.value);
   }
 }
