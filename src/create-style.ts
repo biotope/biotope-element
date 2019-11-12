6136a02708fcffa88ebc3c6277f8494eae94a521
Tiago Marques <tiago.marques@virtual-identity.com>
2019-11-12 17:02:12 +0000

    Add fix

--- src/create-style.ts
+++ src/create-style.ts
@@ -1,10 +1,6 @@
-import { HTMLElementContent } from './types';
+import { HTMLElementContent, HTMLFragment } from './types';
+import { html } from './create-html';
 
-export const createStyle = (styleContent: HTMLElementContent): HTMLStyleElement => {
-  const styleElement = document.createElement('style');
-  styleElement.innerHTML = typeof styleContent === 'string'
-    ? styleContent
-    : styleContent.toString();
-
-  return styleElement;
-};
+export const createStyle = (styleContent: HTMLElementContent): HTMLFragment => html`
+  <style>${typeof styleContent === 'string' ? styleContent : styleContent.toString()}</style>
+`;
