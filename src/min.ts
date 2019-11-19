cc5cdd834b19d0455a51aab0531725040896c4f3
Tiago Marques <tiago.marques@virtual-identity.com>
2019-11-19 17:20:42 +0000

    Add min file initial point

---
+++ src/min.ts
@@ -0,0 +1,6 @@
+/* istanbul ignore next */
+import Component from '.';
+
+/* istanbul ignore next */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).BiotopeElement = Component;
